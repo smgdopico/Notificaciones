06c2959c78c4b8ce2b4f7c69cc422dac92fa7c3e
Sonia <smgdopi@gmail.com>
2021-01-09 14:57:22 +0100

    mandar el audio por un intent

--- app/src/main/java/com/example/notificaciones/ServiceNotificacion.java
+++ app/src/main/java/com/example/notificaciones/ServiceNotificacion.java
@@ -1,43 +1,47 @@
 package com.example.notificaciones;
 
-import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.IBinder;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.JobIntentService;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
-// esto es un service una actuivity sin interfaz
-public class MostrarNotificacionService extends IntentService {
+public class ServiceNotificacion extends JobIntentService {
+
 
     //estas son la variables y los metodos que hemos creado en el mein los hemos traido a esta clase que obliga a externder el intent service
     public final static String CHANNEL_ID = "notificacion";
     public final static int NOTIFICACION_ID = 1;
-    public static final String RUTA_SONIDO ="rutaSonido";
+    public static final int JOB_ID = 1;
 
-    public MostrarNotificacionService() {
-        super("MostrarNotificacionService");
+    public static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, ServiceNotificacion.class, JOB_ID, work);
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    protected void onHandleWork(@NonNull Intent intent) {
         //código para mostrar la notificacion
         //llamamos al metodo  que comprueba el sdk y al que crea la notificacion
-        String ruta=intent.getStringExtra(RUTA_SONIDO);
         creacionCANALNotificacionSuperiorOreo();
-        creaNotificacion(ruta);
+        creaNotificacion();
 
     }
 
     //TODO/////////////////////////////////// METODO HECHO  ////////////////////////////////////////////////////////
     //CREAR LAS ALERTAS DEL MOVIL
-    private PendingIntent abreCtivityEscuhar(String ruta) {
+    private PendingIntent abreCtivityEscuhar() {
         // creamos un intent para ir a nuestra segunda pantalla cuando pulsemos en el icono
         Intent intent = new Intent(getApplicationContext(), OirNotificacionActivity.class);
         // hacemos que si el usuario le da atras vaya a nuestro main o se saldra de app
@@ -45,7 +49,6 @@ public class MostrarNotificacionService extends IntentService {
         // le decimos que pantalla queremos que lo haga
         taskStackBuilder.addParentStack(OirNotificacionActivity.class);
         // le pasamos el intent
-        intent.putExtra(RUTA_SONIDO,ruta);
         taskStackBuilder.addNextIntent(intent);
         // y luego a nuestra variable pending le decimos que coja el taskt y le pasamos esos dos parametros
         //pendingIntent= taskStackBuilder.getPendingIntent(1,PendingIntent.FLAG_UPDATE_CURRENT);
@@ -54,7 +57,7 @@ public class MostrarNotificacionService extends IntentService {
     }
 
     // LE DAMOS FORMA A LA NOTIFICACION COLORES Y DE MAS HISTORIAS, SI LA VERSION ES ANTERIOR A OREO SE EJECUTARA NORMAL SI NO NO FUNCIONARA
-    private void creaNotificacion(String  ruta) {
+    private void creaNotificacion() {
         //T CREAMOS LA NOTIFICACION Y LE PASAMOS EL NANAL
         NotificationCompat.Builder contructor = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
         // CREAMOS EL ICONO QUE APARECERA
@@ -78,7 +81,7 @@ public class MostrarNotificacionService extends IntentService {
         // al contructor le ponemos el content intent y nuestra variable pending
         //contructor.setContentIntent(pendingIntent);
         //llamando directamente el pending intent que devolvemos de arriba
-        contructor.setContentIntent(abreCtivityEscuhar(ruta));
+        contructor.setContentIntent(abreCtivityEscuhar());
         // AHOPRA CREMOS LA VARIABLE
         NotificationManagerCompat notificationCompat = NotificationManagerCompat.from(getApplicationContext());
         notificationCompat.notify(NOTIFICACION_ID, contructor.build());
@@ -96,6 +99,23 @@ public class MostrarNotificacionService extends IntentService {
             notificationManager.createNotificationChannel(notificationChannel);
         }
     }
-
 }
+//todo solo es para el service
+  /*  @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }*/
+/*
+*  {
 
+    public static final int JOB_ID = 1;
+
+    public static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, YourService.class, JOB_ID, work);
+    }
+
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+        // your code
+    }*/
